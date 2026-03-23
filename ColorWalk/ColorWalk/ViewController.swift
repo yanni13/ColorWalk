ae47b38cabd2fd9cabc69911362f7698196a03e7
yanni13 <iumd6075@gmail.com>
2026-03-23 15:04:18 +0900

    프로젝트 초기 세팅 (gitIgnore 및 코드베이스 전환)

---
+++ ColorWalk/ColorWalk/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ColorWalk
+//
+//  Created by 아우신얀 on 3/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        print(#function)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
